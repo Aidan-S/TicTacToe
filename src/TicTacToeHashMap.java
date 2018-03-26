73b0d6dca0d3c7b73b4078e5ff993c7e348376c9
Aidan <Aidan@Aidan-PC>
2018-03-25 22:32:15 -0400

    time to come in for help...

--- src/TicTacToeHashMap.java
+++ src/TicTacToeHashMap.java
@@ -1,5 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.lang.reflect.Field;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class TicTacToeHashMap  {
 
@@ -22,6 +26,28 @@ public class TicTacToeHashMap  {
       return table == null ? 0 : table.length;   
    }
    
+   /**
+	 * @author Aidan-S
+	 * date: March 23th, 2018
+	 * method: create a scanner that I can use to read in Prof. Kelly's files
+	 * @param fname: the name of the file to read
+	 * @param out: the file to print to
+	 * @return: the scanner for the given file
+	 */
+	public static Scanner openWords(String fname) {
+		File file = new File(fname);
+		Scanner input = null;
+		try {
+			input = new Scanner(file);
+		} catch (FileNotFoundException ex) {
+			System.out.println("This isnt there");
+			return null;
+		}
+		return input;	
+	}
+   
+   
+   
    // TODO using the same code to get the table of entries as in the capacity method,
    // create a method that will evaluate the table as directed in the assignment.
    // note - if an entry is not null, then it has a value, it may have more than one value
@@ -32,9 +58,10 @@ public class TicTacToeHashMap  {
                                               IllegalAccessException {
 
       TicTacToeHashMap m = new TicTacToeHashMap();
-
+      Scanner file = openWords(args[0]);
   // TODO read in and store the strings in your hashmap, then close the file
-  
+      
+      
   // TODO print out the capacity using the capcity() method
   // TODO print out the other analytical statistics as required in the assignment
   
